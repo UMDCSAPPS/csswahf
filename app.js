baf7853298ad3d4ad5c1ac8720a70df73938059a
codemastermd <codemastermd@gmail.com>
2016-12-08 15:36:46 -0500

    Basic texting working

---
+++ app.js
@@ -0,0 +1,19 @@
+var credentails = require('./cred.json');
+
+var accountSid = credentails.accountSid; // Your Account SID from www.twilio.com/console
+var authToken = credentails.authToken;   // Your Auth Token from www.twilio.com/console
+
+var twilio = require('twilio');
+var client = new twilio.RestClient(accountSid, authToken);
+
+client.messages.create({
+    body: 'Please reply with your latest grades in each class',
+    to: '+14438785019',  // Text this number
+    from: '+14437762118' // From a valid Twilio number
+}, function(err, message) {
+    if(err){
+      console.log(err)
+    } else {
+      console.log(message.sid);
+    }
+});
