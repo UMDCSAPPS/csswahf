c534874d4fcd031428e28cb9eb39894dff541295
Marcelo McAndrew <marcelomcandrew@gmail.com>
2016-12-09 12:10:30 -0500

    now can read files from temp.json and type name in first field on form

--- webFunctions.js
+++ webFunctions.js
@@ -1,1 +1,35 @@
-// put all the nightmare stuff here
+//Marcelo McAndrew
+
+//require dependencies
+var Nightmare = require('nightmare');
+var nightmare = Nightmare({show:true});
+var fs = require('fs')
+var path = require('path')
+//create new nightmare object
+
+
+var wahf = "https://docs.google.com/forms/d/e/1FAIpQLSdaZ4hnJpch3y76n2_PcLGQEpQUjbDDstGgCGJ3AnzMlNBTTw/viewform"
+
+console.log();
+
+var contents = fs.readFileSync(path.resolve()+'/temp.json');
+// Define to JSON type
+//parse contents e.g post.img, post.title
+var post = JSON.parse(contents);
+
+  nightmare
+    .goto(wahf)
+
+    .screenshot('test.png')
+
+    .type('input:nth-of-type(1)',post.fullname)
+    .click('label:nth-of-type('+post.year+')')
+    .type('input:nth-of-type(4)',post.teamleader)
+
+    .end()
+    .then(function (result) {
+    // console.log(result)
+    })
+    .catch(function (error) {
+      //console.error('Search failed:', error);
+    });
